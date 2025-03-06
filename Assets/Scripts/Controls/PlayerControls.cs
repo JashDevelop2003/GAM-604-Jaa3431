8118c7e3f9c29e4561dcc275ad6f8cfb4d43c083
jaa3431 <jaa3431@my.acm.ac.uk>
2025-03-06 09:54:09 +0000

    Changing back
    Apparently changing the button doesn't affect it at all.

--- Assets/Scripts/Controls/PlayerControls.cs
+++ Assets/Scripts/Controls/PlayerControls.cs
@@ -261,7 +261,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""0480b168-97f8-40ec-a99d-40488767d4ec"",
-                    ""path"": ""<Keyboard>/e"",
+                    ""path"": ""<Keyboard>/q"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
