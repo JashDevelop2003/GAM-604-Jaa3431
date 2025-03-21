eafeb55928bf1769194f3a7027a71be77e2da18c
jaa3431 <jaa3431@my.acm.ac.uk>
2025-03-21 17:02:52 +0000

    Fruits Are Added
    Fruit Effect:
    !Changes the implementation for increasing health and max health upon pickup to a int value to allow each fruit to use the same component rather than creating unique scripts for each fruit.

--- Assets/Scripts/Items/ItemEffects/fruitEffect.cs
+++ Assets/Scripts/Items/ItemEffects/fruitEffect.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cherriesEffect : MonoBehaviour
+public class fruitEffect : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
     [SerializeField] private GameObject player;
     private playerController controller;
     [SerializeField] private bool hasPickup;
+    [SerializeField] int healthValue;
 
     // Cherries Increases Max Health by 3 & Gains 3 Health
     void Awake()
@@ -19,8 +20,8 @@ public class cherriesEffect : MonoBehaviour
         //This ensures that when loading the game again the effect of the relic doesn't play again
         if (!hasPickup)
         {
-            controller.GetModel.MaxHealth += 3;
-            controller.ChangeHealth(3);
+            controller.GetModel.MaxHealth += healthValue;
+            controller.ChangeHealth(healthValue);
             hasPickup = true;
             Debug.Log("Increase Max Health: " +  controller.GetModel.MaxHealth);
         }
