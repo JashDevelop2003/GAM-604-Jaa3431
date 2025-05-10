c06d12994001f3a3d2148a2af42f5c53f02d7b39
jaa3431 <jaa3431@my.acm.ac.uk>
2025-05-10 21:46:19 +0100

    Victory Scene Done (excluding data persistent)
    Victory Scene:
    +Includes a user interface to inform which player has won, the controls to either go back to main menu or quit the game and the current input that will happen when pressing confirm.
    * Requires data file to store the winning player, this means that the scene is currently done at the moment without the challenge of the data persistence.
    
    Navigate Victory:
    + This scripts provides the inputs of left and right to decide the player to head to the main menu or quit the game, the player can then press spacebar to confirm the transition.

---
+++ Assets/Scripts/Menu/Victory/navigateVictory.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class navigateVictory : MonoBehaviour, IDecideLeft, IDecideRight, IConfirm
+{
+    private mainMenuControls menuControls;
+    private int currentChoice;
+    public mainMenuControls MenuControls
+    {
+        get { return menuControls; }
+    }
+
+    [Header("Scene Management")]
+    private sceneManager sceneManager;
+    [SerializeField] private sceneEnum[] scene = new sceneEnum[2];
+
+    [Header("User Interface")]
+    [SerializeField] private TMP_Text inputText;
+    [SerializeField] private string[] displayText = new string[2];
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentChoice = 0;
+        sceneManager = Singleton<sceneManager>.Instance;
+        menuControls = GetComponent<mainMenuControls>();
+        MenuControls.pressedLeft += DecidingLeft;
+        MenuControls.pressedRight += DecidingRight;
+        MenuControls.pressedConfirm += ConfirmingChoice;
+    }
+
+    public void DecidingLeft(object sender, EventArgs e)
+    {
+        currentChoice = 0;
+        ChangeText();
+    }
+
+    public void DecidingRight(object sender, EventArgs e)
+    {
+        currentChoice = 1;
+        ChangeText();
+    }
+
+    public void ConfirmingChoice(object sender, EventArgs e)
+    {
+        sceneManager.ChangeScene(scene[currentChoice]);
+    }
+
+    void ChangeText()
+    {
+        inputText.SetText(displayText[currentChoice]);
+    }
+
+    private void OnDisable()
+    {
+        MenuControls.pressedLeft -= DecidingLeft;
+        MenuControls.pressedRight -= DecidingRight;
+        MenuControls.pressedConfirm -= ConfirmingChoice;
+    }
+}
